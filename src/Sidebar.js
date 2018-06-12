041b16d5c6348b80ea033b598ebc9953d95747b1
Seok Kwun Park <park960@purdue.edu>
2018-06-12 16:01:40 -0400

    style sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,41 +3,63 @@ import React from 'react'
 const Sidebar = (props) => {
     
     return(
-        <aside className="Sidebar">
-            <div className="UserInfo">
-                <div
-                    className="Avatar"
-                    style="background: url(https://api.adorable.io/avatars/32/davey@getfretless.com)"
-                ></div>
-                <div className="user">Davey</div>
-                    <a href="#">
-                        <i className="fas fa-sign-out-alt"></i>
-                    </a>
+        <aside
+            className="Sidebar"
+            style={styles.sidebar}
+        >
+            <div
+                className="UserInfo"
+                style={styles.children}
+            >
+                <div className="Avatar"></div>
+                <div className="user">
+                    {props.user.userName}
                 </div>
-                <h1>XTBC 18</h1>
-                <nav className="RoomList">
-                    <h2>Rooms</h2>
-                    <ul>
-                        <li><a href="#">general</a></li>
-                        <li><a href="#">random</a></li>
-                    </ul>
-                </nav>
+                <a href="#">
+                    <i className="fas fa-sign-out-alt"></i>
+                </a>
+            </div>
+            <h1
+                style={{
+                    ...styles.children,
+                    ...styles.h1,
+                }}
+            >
+                XTBC 18
+            </h1>
+                <nav
+                    className="RoomList"
+                    style={styles.children}
+                >
+                <h2>Rooms</h2>
+                <ul>
+                    <li><a href="#">general</a></li>
+                    <li><a href="#">random</a></li>
+                </ul>
+            </nav>
         </aside>
     )    
     
 }
 
-const style = {
+const styles = {
     sidebard: {
-
+        backgroundColor: '#333344',
+        color: 'rgba(255, 255, 255, 0.8)',
+        width: '12rem',
+        padding: '1rem 0',
+        display: 'flex',
+        flexDirection: 'column',
     },
     
     children: {
-
+        padding: '0 1rem',
     },
 
     h1: {
-
+        color: 'white',
+        fontSize: '1.2rem',
+        marginTop: 0,
     },
 }
 
