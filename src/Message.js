6575a5c554fd6041f1598d12da496677e63e218e
Seok Kwun Park <park960@purdue.edu>
2018-06-12 16:25:17 -0400

    style Message

--- src/Message.js
+++ src/Message.js
@@ -3,10 +3,26 @@ import React from 'react'
 const Message = (props) => {
 
     return(
-        <div className="Message">
-            {props.message.userName}: {props.message.body}
+        <div className="Message" style={styles.Message}>
+            <div className="Detail" style={styles.Details}>
+                {props.message.userName}: {props.message.body}
+            </div>
         </div>
     )
 }
+
+const styles = {
+    Message: {
+        display: 'flex',
+        marginTop: '1rem',
+        padding: '0 1rem',
+    },
+      
+    Details: {
+        flex: '1',
+        paddingLeft: '0.5rem',
+    },
+}
+
 
 export default Message
