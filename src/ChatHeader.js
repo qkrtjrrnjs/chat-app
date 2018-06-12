dbe7721b27ccac9588e103a285529e171b697921
Seok Kwun Park <park960@purdue.edu>
2018-06-12 16:49:54 -0400

    style ChatHeader

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -2,13 +2,38 @@ import React from 'react'
 
 const ChatHeader = () => {
     return(
-        <div className="ChatHeader">
+        <div 
+            className="ChatHeader"
+            style={styles.ChatHeader}
+        >
             <div className="roomInfo">
-                <h2>#general</h2>
-                <p>Announcements and general chat</p>
+                <h2 style={styles.H2}>#general</h2>
+                <p style={styles.P}>Announcements and general chat</p>
             </div>
         </div>
     )
+}
+
+const styles = {
+    ChatHeader: {
+        backgroundColor: '#f3f3f3',
+        borderBottom: '1px solid #ccc',
+        height: '3rem',
+        padding: '0 1rem',
+        display: 'flex',
+        alignItems: 'center',
+    },
+      
+    H2: {
+        fontSize: '1.1rem',
+        margin: '0',
+    },
+    
+    P: {
+        color: '#999',
+        margin: '0',
+        fontSize: '0.8rem',
+    },
 }
 
 export default ChatHeader
