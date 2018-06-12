ab9babd28678eb0f0ee69f76ba84d504caa888d0
Seok Kwun Park <park960@purdue.edu>
2018-06-12 16:35:34 -0400

    style MessageForm

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -20,6 +20,7 @@ class MessageForm extends Component{
             <form 
                 className="MessageForm"
                 onSubmit={this.handleSubmit}
+                style={styles.MessageForm}
             >
                 <input 
                     type="text" 
@@ -27,11 +28,61 @@ class MessageForm extends Component{
                     placeholder="Enter a message..." 
                     value={this.state.body}
                     onChange={this.handleChange}
+                    style={styles.Input}
                 />
-                <button type="submit">send</button>
+                <button type="submit" style={styles.Button}>send</button>
             </form>
         )
     }
+}
+
+const styles = {
+    MessageForm: {
+        backgroundColor: 'white',
+        height: '3rem',
+        display: 'flex',
+        alignItems: 'stretch',
+        border: '2px solid #999',
+        borderRadius: '0.5rem',
+        margin: '0.25rem',
+        padding: '0',
+      },
+      
+     /*.MessageForm .chatIcon*/
+     ChatIcon: {
+        display: 'flex',
+        borderRadius: '0.5rem',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        color: '#ccc', 
+        padding: '0 0.5rem',
+        fontSize: '1.2rem',
+      },
+
+      /*MessageForm input*/
+      Input: {
+        flex: '1',
+        fontSize: '1.2rem',
+        border: '0',
+      },
+
+      /*
+      .MessageForm input:focus {
+        outline: 0;
+      },
+      */
+      
+      /*.MessageForm button*/
+       Button: {
+        fontSize: '1.5rem',
+        backgroundColor: '#1A8FE3',
+        color: 'white',
+        paddingLeft: '1rem',
+        paddingRight: '1rem',
+        borderTopRightRadius: '0.5rem',
+        borderBottomRightRadius: '0.5rem',
+        border: '1px solid white',
+      },
 }
 
 export default MessageForm
