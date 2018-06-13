d34756162c489535fc2a2380a47a9cbff09c999f
Seok Kwun Park <park960@purdue.edu>
2018-06-13 09:56:36 -0400

    styling sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Userinfo from './Userinfo'
+import RoomList from './RoomList'
 
 const Sidebar = (props) => {
     
@@ -10,36 +11,8 @@ const Sidebar = (props) => {
             style={styles.sidebar}
         >
             <Userinfo user={props.user} />
-            <h1
-                style={{
-                    ...styles.children,
-                    ...styles.h1,
-                }}
-            >
-                XTBC 18
-            </h1>
-            <div
-                className="UserInfo"
-                style={styles.children}
-            >
-                <div className="Avatar"></div>
-                <div className="user">
-                    {props.user.userName}
-                </div>
-                <a href="#">
-                    <i className="fas fa-sign-out-alt"></i>
-                </a>
-            </div>
-                <nav
-                    className="RoomList"
-                    style={styles.children}
-                >
-                    <h2>Rooms</h2>
-                    <ul>
-                        <li><a href="#">general</a></li>
-                        <li><a href="#">random</a></li>
-                    </ul>
-            </nav>
+            <h1 style={styles.h1}>XTBC 18</h1>
+            <RoomList />
         </aside>
     )    
     
@@ -47,23 +20,21 @@ const Sidebar = (props) => {
 
 const styles = {
     sidebar: {
-        backgroundColor: '#6991ac',
-        color: '#f5f5f5',
-        width: '15rem',
+        backgroundColor: '#333344',
+        color: 'rgba(255, 255, 255, 0.8)',
+        width: '12rem',
         padding: '1rem 0',
         display: 'flex',
         flexDirection: 'column',
-    },
+        width: '17vw',
+      },
     
-    children: {
-        padding: '0 1rem',
-    },
-
-    h1: {
-        color: '#f5f5f5',
+      h1: {
+        color: 'white',
         fontSize: '1.2rem',
         marginTop: 0,
-    },
+        padding: '0 0.3rem',
+      },
 }
 
 export default Sidebar
