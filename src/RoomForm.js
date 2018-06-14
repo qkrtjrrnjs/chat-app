fc8c22b42bcddf2ca3c4f059dac40737130a6e4d
Seok Kwun Park <park960@purdue.edu>
2018-06-14 11:11:52 -0400

    refactor roomform state

--- src/RoomForm.js
+++ src/RoomForm.js
@@ -2,22 +2,21 @@ import React, {Component} from 'react'
 
 class RoomForm extends Component{
     state = {
-        name: '',
-        description: '',
+        room: {
+            name: '',
+            description: '',
+        }
     }
 
     handleChange = (ev) =>{
-        const change = []
+        const change = [...this.state.room]
         change[ev.target.name] = ev.target.value
         this.setState(change)
     }
 
     handleSubmit = (ev) => {
         ev.preventDefault()
-        this.props.addRoom({
-            name: this.state.name,
-            description: this.state.description,
-        })
+        this.props.addRoom(this.state.room)
     }
 
     render(){
@@ -26,11 +25,11 @@ class RoomForm extends Component{
                 <form onSubmit={this.handleSubmit}>
                     <p>
                         <label htmlFor="name">Room name</label>
-                        <input type="text" name="name" value={this.state.name} autoFocus onChange={this.handleChange}/>
+                        <input type="text" name="name" value={this.state.room.name} autoFocus onChange={this.handleChange}/>
                     </p>
                     <p>
                         <label htmlFor="description">Description</label>
-                        <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
+                        <input type="text" name="description" value={this.state.room.description} onChange={this.handleChange}/>
                     </p>
                     <div>
                         <button type="button" onClick={this.props.hideFormRoom}>cancel</button>
