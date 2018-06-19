75432a94d71eb26b40435094a410c53e06803939
Seok Kwun Park <park960@purdue.edu>
2018-06-19 10:07:33 -0400

    use avatars from auth services, if available

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Avatar = ({ user, style }) =>{
+    const photoUrl = user.photoUrl || `https://api.adorable.io/avatars/40/${user.email}`
     return(
         <div
             className="Avatar"
             style={{
                 ...styles,
                 ...style,
-            background: `url(https://api.adorable.io/avatars/40/${user.email})`,
+                backgroundImage: `url(${photoUrl})`,
             }}
         ></div>
     )
@@ -20,4 +21,5 @@ const styles = {
     width: '40px',
     fontSize: '1rem',
     borderRadius: '20px',
+    backgroundSize: '40px',
 }
